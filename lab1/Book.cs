e3be662028c82a6e74631d4862e774e6c8ef8a7a
Kamar <kmprojects1998@gmail.com>
2020-09-04 23:27:18 +0300

    Created class Book.

---
+++ lab1/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Book
+    {
+        public int ISBN { get; set; }
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Publisher { get; set; }
+
+    }
+}
