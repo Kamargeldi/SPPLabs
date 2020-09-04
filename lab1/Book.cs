45ee7251b088b6154c56fd2d1838e0d6d99c49cc
Kamar <kmprojects1998@gmail.com>
2020-09-05 00:24:07 +0300

    Overload equality operators.

--- lab1/Book.cs
+++ lab1/Book.cs
@@ -22,9 +22,9 @@ namespace lab1
         public int CompareTo(Book other)
         {
             if (other is null)
-                throw new ArgumentNullException(nameof(other));
+                return 1;
 
-            return Name.CompareTo(other);
+            return Name.CompareTo(other.Name);
         }
 
         /// <summary>
@@ -35,11 +35,40 @@ namespace lab1
         public bool Equals(Book other)
         {
             if (other is null)
-                throw new ArgumentNullException(nameof(other));
+                return false;
 
             return this.Author == other.Author &&
                    this.Name == other.Name &&
                    this.Publisher == other.Publisher;
+        }
+
+        /// <summary>
+        /// Equality operator.
+        /// </summary>
+        /// <param name="leftSide">First instance.</param>
+        /// <param name="rightSide">Second instance.</param>
+        /// <returns>Returns if both instances are equal.</returns>
+        public static bool operator ==(Book leftSide, Book rightSide)
+        {
+            if (leftSide is null && rightSide is null)
+                return true;
+
+            else if (leftSide is null)
+                return rightSide.Equals(leftSide);
+            else
+                return leftSide.Equals(rightSide);
+
+        }
+
+        /// <summary>
+        /// Not Equality operator.
+        /// </summary>
+        /// <param name="leftSide">First instance.</param>
+        /// <param name="rightSide">Second instance.</param>
+        /// <returns>Returns if both instances are not equal.</returns>
+        public static bool operator !=(Book leftSide, Book rightSide)
+        {
+            return !(leftSide == rightSide);
         }
 
     }
